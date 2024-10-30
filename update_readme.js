9e11e7bc00b24362dd15b5fe38345381bc5a0726
Psakejs <patpampam4@gmail.com>
2024-10-29 20:29:06 -0500

    Update update_readme.js

--- update_readme.js
+++ update_readme.js
@@ -11,7 +11,7 @@ const recentCommits = activityData
     const commitCount = event.payload.commits.length;
     return `- **${repoName}**: ${commitCount} commit(s)`;
   })
-  .slice(0, 5); // Limita la cantidad de actividades que deseas mostrar (en este caso, 5)
+  .slice(0, 6); // Limita la cantidad de actividades que deseas mostrar (en este caso, 5)
 
 // Actualiza el README.md con la actividad reciente
 let readmeContent = fs.readFileSync('README.md', 'utf-8');
